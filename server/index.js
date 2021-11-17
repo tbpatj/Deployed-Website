6496db2fd7b62c9225407f091ab2b6810fc80ff7
tbpatj <timothy@Timothys-MBP.localdomain>
2021-11-17 14:06:38 -0700

    added javascript file and css file

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
+app.use('/css', express.static(path.join(__dirname, '../client/stylesheet.css')));
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
