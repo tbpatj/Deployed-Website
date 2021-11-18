32d880aa8ae3abcf85021cff3def775c6e143196
tbpatj <timothy@Timothys-MacBook-Pro.local>
2021-11-17 19:09:07 -0700

    added some stuff...

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-console.log("yeeeah");
+let navBttn = document.getElementById("nav-bttn");
+let navCntn = document.getElementById("nav-container");
+
+navBttn.addEventListener("click",addNavClass);
+
+function addNavClass(event){
+    console.log("yeeeah");
+        navCntn.classList.toggle("display-nav");
+        // navCntn.classList.toggle("hidden");
+}
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 4005;
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
-
 app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
 app.use('/css', express.static(path.join(__dirname, '../client/stylesheet.css')));
 
