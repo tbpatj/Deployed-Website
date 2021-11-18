9e2aeb014533be5cc1c346c1daafaa672dc62b37
tbpatj <timothy@Timothys-MacBook-Pro.local>
2021-11-17 19:46:48 -0700

    fixed some navbar stuff

--- client/main.js
+++ client/main.js
@@ -1,10 +1,24 @@
 let navBttn = document.getElementById("nav-bttn");
 let navCntn = document.getElementById("nav-container");
+let navBar = document.getElementById("nav-bar-3");
+navBar.addEventListener("transitionend",createAnimation);
 
 navBttn.addEventListener("click",addNavClass);
+let toggledNav = false;
 
 function addNavClass(event){
     console.log("yeeeah");
-        navCntn.classList.toggle("display-nav");
+        navBar.classList.add("nav-bar-anim");
+        toggledNav = !toggledNav;
+        
         // navCntn.classList.toggle("hidden");
+}
+function createAnimation(event){
+    if(navBar.classList.contains("nav-bar-anim")){
+        navBar.classList.toggle("nav-bar-anim");
+        navBar.classList.toggle('nav-bar-anim2');
+        
+    }else if(navBar.classList.contains("nav-bar-anim2") && toggledNav){
+        navCntn.classList.add("display-nav");
+    }
 }
